0cf4d9749bcb2a7e0201c17dfe4a7b3e0096f485
Saicharan Pagidimunthala <saicharan8999@gmail.com>
2024-06-01 19:29:18 -0500

    flutter

---
+++ adv_basic/lib/quiz.dart
@@ -0,0 +1,46 @@
+import 'package:adv_basic/questions_screen.dart';
+import 'package:flutter/material.dart';
+import 'package:adv_basic/start_screen.dart';
+
+class Quiz extends StatefulWidget {
+  const Quiz({super.key});
+  @override
+  State<Quiz> createState() {
+    return _QuizState();
+  }
+}
+
+class _QuizState extends State<Quiz> {
+  var activeScreen = 'start-screen';
+
+  void switchScreen() {
+    setState(() {
+      activeScreen = 'questions-screen';
+    });
+  }
+
+  @override
+  Widget build(context) {
+    Widget screenWidget = StartScreen(switchScreen);
+    if (activeScreen == 'questions-screen') {
+      screenWidget = const QuestionsScreen();
+    }
+    return MaterialApp(
+      home: Scaffold(
+        body: Container(
+          decoration: const BoxDecoration(
+            gradient: LinearGradient(
+              colors: [
+                Color.fromARGB(255, 74, 25, 158),
+                Color.fromARGB(255, 126, 68, 225),
+              ],
+              begin: Alignment.topLeft,
+              end: Alignment.bottomRight,
+            ),
+          ),
+          child: screenWidget,
+        ),
+      ),
+    );
+  }
+}
